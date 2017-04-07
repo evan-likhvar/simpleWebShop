11915abed06fce18ffd1ffdba1193e097de47a21
evan <elk@gmail.com>
2017-04-07 17:07:18 +0300

    coper v1.3

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\URL;
 class FrontController extends Controller
 {
 
-    public function __construct()
+/*    public function __construct()
     {
 
             $this->middleware('auth');
-    }
+    }*/
 
     protected function getCartInfo():array {
         $cartInfo = array();

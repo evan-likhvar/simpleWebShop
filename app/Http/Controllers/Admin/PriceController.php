53812b9677ef8b75a6e402d0073a8ded49cfac60
evan <elk@gmail.com>
2017-04-10 19:56:24 +0300

    coper v1.3 add priceUA

--- app/Http/Controllers/Admin/PriceController.php
+++ app/Http/Controllers/Admin/PriceController.php
@@ -113,49 +113,46 @@ XML;
     public function priceUAPriceXML()
     {
         $xmlstr = <<<XML
-<?xml version="1.0" encoding="UTF-8" ?>
-<price>
-    <date>fyjuy</date>
-    <firmName>Мой КуперХантер</firmName>
-    <firmId>1234</firmId>
-    <categories>
-        <category>
-            <id>1</id>
-            <name>Бытовая техника</name>
-        </category>
-        <category>
-            <id>2</id>
-            <parentId>1</parentId>
-            <name>Климатическая техника</name>
-        </category>
-		<category>
-            <id>3</id>
-            <parentId>2</parentId>
-            <name>Кондиционеры</name>
-        </category>
-    </categories>
-    <items>
-    </items>
+<?xml version="1.0" encoding="UTF-8"?>
+<price date="2008-09-19 12:55">
+<name>Куперхантер.укр</name>
+<catalog>
+	<category id="1">Бытовая техника</category>
+	<category id="10" parentID="1">Климатическое оборудование</category>
+	<category id="100" parentID="10">Кондиционеры, увлажнители, воздухоочистители</category>
+</catalog>
+<items>
+</items>
 </price>
 XML;
         $hotLinePriceXML = new SimpleXMLElement($xmlstr);
 
-        $hotLinePriceXML->date = date("Y-m-d H:i");
+        //$hotLinePriceXML->date = date("Y-m-d H:i");
 
-        $articles = Article::where('priceua','=','1')->whereIn('category_id',[2,3,4,5,6,7,8])->get();
+        $date = $hotLinePriceXML->attributes();
+        $date['date'] = date("Y-m-d H:i");
+
+//return dd(date("Y-m-d H:i"),$hotLinePriceXML,$tmp['date'],$hotLinePriceXML);
+
+
+        $articles = Article::where('priceua','=','1')->whereIn('category_id',[2])->get();
         foreach ($articles as $article) {
             $item = $hotLinePriceXML->items->addChild('item');
-            $item->addChild('id', $article->id);
+            $item['id'] = $article->id;
+          //  $item->addChild('id', $article->id);
+            $item->addChild('name', str_replace('&','&amp;',$article->name));
+            $item->addChild('categoryId', 100);
+            $item->addChild('priceRUAH', $article->priceGRN);
+            $item->addChild('url', 'http://www.куперхантер.укр/купить/'.str_replace('&','&amp;',$article->getArticleLink()));
+            $item->addChild('image', "http://www.куперхантер.укр".$article->getIntroImg('M'));
+            $item->addChild('vendor', str_replace('&','&amp;',$article->Vendor->name));
+            $item->addChild('description', str_replace('&','&amp;', strip_tags( $article->description)));
+
+/*
             if (isset($article->nomer)&&!empty($article->nomer)) {
                 $item->addChild('code', $article->nomer);
             }
-            $item->addChild('vendor', str_replace('&','&amp;',$article->Vendor->name));
-            $item->addChild('name', str_replace('&','&amp;',$article->name));
-            $item->addChild('description', str_replace('&','&amp;',$article->name));
-            $item->addChild('url', 'http://www.куперхантер.укр/купить/'.str_replace('&','&amp;',$article->getArticleLink()));
-            $item->addChild('image', "http://www.куперхантер.укр".$article->getIntroImg('M'));
-            $item->addChild('priceRUAH', $article->priceGRN);
-            $item->addChild('stock', 'В наличии');
+            $item->addChild('stock', 'В наличии');*/
         }
         $path = public_path().'/XML/priceUA.xml';
         fclose(fopen($path, "a+t"));
